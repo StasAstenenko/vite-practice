604a07575883d5caac50b5fbdbed6c6d3b8486fd
Stas_Astienenko <stasastenenko8@gmail.com>
2024-06-09 16:21:15 +0300

    kfd

--- src/js/switcher.js
+++ src/js/switcher.js
@@ -1,5 +1,21 @@
 const switcherElem = document.querySelector('.switcher-toggle');
 
 switcherElem.addEventListener('change', event => {
-  console.log(event.target.checked);
+  if (event.target.checked) {
+    document.body.classList.add('dark');
+    document.body.classList.remove('light');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.body.classList.add('light');
+    document.body.classList.remove('dark');
+    localStorage.setItem('theme', 'light');
+  }
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark');
+    document.body.classList.remove('light');
+    switcherElem.checked = true;
+  } 
+})
