0ad9edbee7c8c8fbbfbb545a5fdb83eaaded6175
Stas_Astienenko <stasastenenko8@gmail.com>
2024-06-23 15:32:07 +0300

    task2

--- src/js/UnsplashAPI.js
+++ src/js/UnsplashAPI.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 export class UnsplashAPI {
   #BASE_URL = 'https://api.unsplash.com';
   #API_KEY = 'gcevo00lZKvSMKLnZZJPKYS5xNbpbsP_4i6E-BVlG58';
+  #query = '';
+
   async getPopularPhotos(page) {
     const url = `${
       this.#BASE_URL
@@ -15,5 +17,23 @@ export class UnsplashAPI {
     } catch (err) {
       console.log(err);
     }
+  }
+
+  async searchPhoto(page) {
+    const url = `${
+      this.#BASE_URL
+    }/search/photos?page=${page}&query=${this.#query}&per_page=12&orientation=portrait&client_id=${
+      this.#API_KEY
+    }`;
+    try {
+      const { data } = await axios(url);
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  set query(newQuery) {
+    this.#query = newQuery;
   }
 }
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,12 +1,45 @@
 import iziToast from 'izitoast';
 import { UnsplashAPI } from './UnsplashAPI';
 import { galleryTemplate } from './render_function';
+import Pagination from 'tui-pagination'; /* ES6 */
+import 'tui-pagination/dist/tui-pagination.min.css';
 
 const api = new UnsplashAPI();
 const galleryList = document.querySelector('.js-gallery');
+const container = document.getElementById('tui-pagination-container');
+const form = document.querySelector('.js-search-form');
+
+
+
+const options = { // below default value of options
+  totalItems: 0,
+  itemsPerPage: 12,
+  visiblePages: 5,
+  page: 1,
+}
+const pagination = new Pagination(container, options);
+
+const currentPage = pagination.getCurrentPage();
+
 
 api
-  .getPopularPhotos(1)
+  .getPopularPhotos(currentPage)
+  .then(data => {
+    const markup = galleryTemplate(data.results);
+    galleryList.innerHTML = markup;
+    pagination.reset(data.total);
+  })
+  .catch(err => {
+    console.log(err);
+    iziToast.error({
+      message: 'Error',
+    });
+  });
+
+pagination.on('afterMove', (event) => {
+    const currentPage = event.page;
+    api
+  .getPopularPhotos(currentPage)
   .then(data => {
     const markup = galleryTemplate(data.results);
     galleryList.innerHTML = markup;
@@ -16,4 +49,29 @@ api
     iziToast.error({
       message: 'Error',
     });
-  });
+  }); 
+});
+
+form.addEventListener('submit', async e => {
+  e.preventDefault();
+  const inputValue = e.target.elements.query.value.trim();
+
+  if (inputValue === '') {
+    iziToast.warning({
+      message: 'Enter photo name',
+    });
+    return;
+  };
+
+  api.query = inputValue;
+  try {
+    const data = await api.searchPhoto(currentPage);
+    const markup = galleryTemplate(data.results);
+    galleryList.innerHTML = markup;
+    pagination.reset(data.total);
+  } catch (err) {
+  console.log(err);
+  };
+
+
+});
